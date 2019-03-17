f71fc5b3bc1b860dd903df5b9932cf4cd498c9f9
Jonathan <alcidescp@hotmail.com>
2019-03-16 18:10:43 -0600

    Entrada

---
+++ Guia/src/Entrada.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Entrada {
+      public int exponente(){
+        return Integer.parseInt(JOptionPane.showInputDialog("Introduzca el exponente"));
+        
+    }
+    public float base(){
+        return Float.parseFloat(JOptionPane.showInputDialog("Introduzca el exponente"));
+    
+    }
+    
+}
