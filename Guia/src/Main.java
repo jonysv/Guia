6fb72a5632388a2c16d8e3155b7578c17b052bb0
Jonathan <alcidescp@hotmail.com>
2019-03-16 18:31:12 -0600

    Master

---
+++ Guia/src/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Main {
+     public static void main(String[] args) {
+         Entrada A = new Entrada();
+         Procesamiento B = new Procesamiento();
+         B.procesamiento(A.base(),A.exponente());
+        // TODO code application logic here
+    }
+    
+    
+}
